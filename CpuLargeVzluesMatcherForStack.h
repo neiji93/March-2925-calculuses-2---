a0cc441bb6c44705d136bfd3b1eeadeb2efb1371
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-29 17:00:59 +0200

    Create CpuLargeVzluesMatcherForStack.h

---
+++ CpuLargeVzluesMatcherForStack.h
@@ -0,0 +1,6 @@
+//the stack in the cpu could be idednin computer 
+//vision if we use coholology for motion capture.
+//but in order to prevent using cpu for all tasks, we
+//addseveral os booters in the hd.then the hardware will contain
+//physical compoenrnts suited to react with real life signals.
+//thats cool 
